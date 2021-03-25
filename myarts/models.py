6984a13cc65170aa21dbe24214417a34c1bafada
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-25 18:26:20 +0100

    created models for data storage

--- myarts/models.py
+++ myarts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(
+        max_length=250,
+        help_text='Enter the title of your article',
+        validators=[MinLengthValidator(
+            1, "Title must have more than 1 character"
+        )]
+    )
+    text = models.TextField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
